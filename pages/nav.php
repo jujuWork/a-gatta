5ad9f634d99f4832c0a1b8885ed1f31846b2f154
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-16 15:59:42 +0900

    nav and readme

--- pages/nav.php
+++ pages/nav.php
@@ -1,3 +1,31 @@
 <nav>
-    NAVIGATION
+    <a href="#home">
+        <h1>SPONAGO</h1>
+    </a>
+    <ul>
+        <li>
+            <a href="#forum">Forum</a>
+        </li>
+        <li>
+            <a href="#product">Production</a>
+        </li>
+        <li>
+            <a href="#manual">Manual</a>
+        </li>
+        <li>
+            <a href="#management">Management</a>
+        </li>
+        <li>
+            <a href="#shift">Shift</a>
+        </li>
+        <li>
+            <a href="#daily">Daily Report</a>
+        </li>
+        <li>
+            <a href="#case">Case Management</a>
+        </li>
+        <li>
+            <a href="#user">User</a>
+        </li>
+    </ul>
 </nav>
