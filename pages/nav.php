a6a2ece1e4c5df65c6d0de1145f6985c41e69937
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-16 18:02:21 +0900

    hover

--- pages/nav.php
+++ pages/nav.php
@@ -4,28 +4,45 @@
     </a>
     <ul>
         <li>
-            <a href="#forum">Forum</a>
+            <a href="#forum">
+                <img src="<?php echo htmlspecialchars(get_template_directory_uri() . './assets/icons/forum.png') ?>" alt="forum">
+                Forum
+            </a>
         </li>
         <li>
-            <a href="#product">Production</a>
+            <a href="#product">
+                <img src="<?php echo htmlspecialchars(get_template_directory_uri() . './assets/icons/production.png') ?>" alt="production">
+                Production</a>
         </li>
         <li>
-            <a href="#manual">Manual</a>
+            <a href="#manual">
+                <img src="<?php echo htmlspecialchars(get_template_directory_uri() . './assets/icons/manual.png') ?>" alt="manual">
+                Manual</a>
         </li>
         <li>
-            <a href="#management">Management</a>
+            <a href="#management">
+                <img src="<?php echo htmlspecialchars(get_template_directory_uri() . './assets/icons/management.png') ?>" alt="management">
+                Management</a>
         </li>
         <li>
-            <a href="#shift">Shift</a>
+            <a href="#shift">
+                <img src="<?php echo htmlspecialchars(get_template_directory_uri() . './assets/icons/shift.png') ?>" alt="shift">
+                Shift</a>
         </li>
         <li>
-            <a href="#daily">Daily Report</a>
+            <a href="#daily">
+                <img src="<?php echo htmlspecialchars(get_template_directory_uri() . './assets/icons/daily.png') ?>" alt="daily">
+                Daily Report</a>
         </li>
         <li>
-            <a href="#case">Case Management</a>
+            <a href="#case">
+                <img src="<?php echo htmlspecialchars(get_template_directory_uri() . './assets/icons/case.png') ?>" alt="case">
+                Case Management</a>
         </li>
         <li>
-            <a href="#user">User</a>
+            <a href="#user">
+                <img src="<?php echo htmlspecialchars(get_template_directory_uri() . './assets/icons/user.png') ?>" alt="user">
+                User</a>
         </li>
     </ul>
 </nav>
