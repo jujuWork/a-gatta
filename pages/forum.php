34325d255755c3e21c11eeb315ea798574b04642
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-18 15:10:06 +0900

    forum

---
+++ pages/forum.php
@@ -0,0 +1,12 @@
+<main>
+    <div class="forum">
+        <h2>Forum</h2>
+        <p>フォーラム</p>
+    </div>
+    <div class="forum__container">
+        <div class="forum__container-items">
+            <p>写真フリー素材</p>
+            <div>img</div>
+        </div>
+    </div>
+</main>
