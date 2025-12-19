d34318c6fffcf0ec6c58ab0de0e7bf732a73d424
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-19 16:40:26 +0900

    product

--- pages/product.php
+++ pages/product.php
@@ -1,19 +1,30 @@
 <main>
     <div class="product__container">
-        <div class="product__container-item">
-            <a href="#odan">
-                <img src="/assets/icons/product/o-dan.png" alt="odan">
-            </a>
+        <div class="product__container-nav">
+            <ul>
+                <li>web design</li>
+                <li>web coding</li>
+                <li>illustrator</li>
+                <li>DTP</li>
+                <li>LOGO</li>
+            </ul>
         </div>
         <div class="product__container-item">
-            <a href="#odan">
-                <img src="/assets/icons/product/o-dan.png" alt="odan">
-            </a>
+            <div class="product__container-item-list">
+                <h3>Web Design</h3>
+            </div>
         </div>
         <div class="product__container-item">
-            <a href="#odan">
-                <img src="/assets/icons/product/o-dan.png" alt="odan">
-            </a>
+            TWO
+        </div>
+        <div class="product__container-item">
+            THREE
+        </div>
+        <div class="product__container-item">
+            FOUR
+        </div>
+        <div class="product__container-item">
+            FIVE
         </div>
     </div>
 </main>
