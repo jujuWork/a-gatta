367298eee0baf6a547e3beda9d2319146b678710
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-19 14:36:39 +0900

    product

---
+++ pages/product.php
@@ -0,0 +1,19 @@
+<main>
+    <div class="product__container">
+        <div class="product__container-item">
+            <a href="#odan">
+                <img src="/assets/icons/product/o-dan.png" alt="odan">
+            </a>
+        </div>
+        <div class="product__container-item">
+            <a href="#odan">
+                <img src="/assets/icons/product/o-dan.png" alt="odan">
+            </a>
+        </div>
+        <div class="product__container-item">
+            <a href="#odan">
+                <img src="/assets/icons/product/o-dan.png" alt="odan">
+            </a>
+        </div>
+    </div>
+</main>
