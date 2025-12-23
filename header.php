b261f0026e0ff71085297d408c42dec6e874d301
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-23 16:34:53 +0900

    navigation

--- header.php
+++ header.php
@@ -1,1 +1,41 @@
-<h1>header</h1>
+<header>
+    <a href="<?php echo esc_url(home_url('/index.php')); ?>">
+        <h1>A型
+            <span>Sponago</span>
+        </h1>
+    </a>
+    <nav>
+        <ul>
+            <li>
+                <a href="<?php echo esc_url(home_url('/index')); ?>">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo esc_url(home_url('/products')); ?>">
+                    Products
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo esc_url(home_url('/manual')); ?>">
+                    Manuals
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo esc_url(home_url('/shift')); ?>">
+                    Shifts
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo esc_url(home_url('/daily')); ?>">
+                    Daily
+                </a>
+            </li>
+            <li>
+                <a href="<?php echo esc_url(home_url('/case')); ?>">
+                    Case
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
