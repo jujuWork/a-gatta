6af216d6410253d026b78001d54499bbb1e67fb8
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-23 16:37:07 +0900

    navigation

---
+++ pages/nav.php
@@ -0,0 +1,47 @@
+<nav>
+    <ul>
+        <li>
+            <a href="<?php echo esc_url(home_url('./index.php')); ?>">
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/icons/home.png') ?>" alt="home">
+                Home
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo esc_url(home_url('/production')); ?>">
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/icons/production.png') ?>" alt=" production">
+                Production
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo esc_url(home_url('/manual')); ?>">
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/icons/manual.png') ?>" alt="manual">
+                Manual
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo esc_url(home_url('/management')); ?>">
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/icons/management.png') ?>" alt="management">
+                Management
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo esc_url(home_url('/shift')); ?>">
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/icons/shift.png') ?>" alt="shift">
+                Shift
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo esc_url(home_url('/daily')); ?>">
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/icons/daily.png') ?>" alt="daily">
+                Daily Report
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo esc_url(home_url('/case')); ?>">
+                <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/icons/case.png') ?>" alt="case">
+                Case Management
+            </a>
+        </li>
+    </ul>
+
+</nav>
