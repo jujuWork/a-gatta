23eb86f38422ab32677864dfde53842f7b8fea19
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-24 16:03:39 +0900

    footer

--- footer.php
+++ footer.php
@@ -4,16 +4,36 @@
             <h2>Sponago</h2>
         </div>
         <div class="footer__item-branch">
-            <div class="footer__item-office">B gata</div>
-            <div class="footer__item-office">A gata</div>
-            <div class="footer__item-office">Transportation</div>
-            <div class="footer__item-office">Support Office</div>
-            <div class="footer__item-office">Living Support</div>
-            <div class="footer__item-office">Local Activities</div>
-            <div class="footer__item-office">Daily Living</div>
-            <div class="footer__item-office">Nurse</div>
+            <div class="footer__item-office">
+                <a href="#" class="footer__link">B型</a>
+            </div>
+            <div class="footer__item-office">
+                <a href="#" class="footer__link">A型</a>
+            </div>
+            <div class="footer__item-office">
+                <a href="#" class="footer__link">就労移行支</a>
+            </div>
+            <div class="footer__item-office">
+                <a href="#" class="footer__link">就労選択支</a>
+            </div>
+            <div class="footer__item-office">
+                <a href="#" class="footer__link">生活共同</a>
+            </div>
+            <div class="footer__item-office">
+                <a href="#" class="footer__link">地域活動</a>
+            </div>
+            <div class="footer__item-office">
+                <a href="#" class="footer__link">生活介護</a>
+            </div>
+            <div class="footer__item-office">
+                <a href="#" class="footer__link">訪問介護</a>
+            </div>
+        </div>
+        <div class="footer__date">
+            <h4>© All Rights Reserved 2025</h4>
         </div>
     </div>
+
 </footer>
 
 
