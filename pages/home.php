edb14515f239b2ee3582b36c5325cc4b48b08256
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-24 16:42:17 +0900

    content

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,20 @@
+<?php get_header(); ?>
+
 <main>
-    <h1>home content</h1>
+    <div class="main__container">
+        <div class="main__title">
+            <h2>Sponago Top Team</h2>
+            <div class="main__title-texts">
+                <p>スポナゴA型就労継続支援のポータルサイト</p>
+            </div>
+            <div class="main__title-texts">
+                <p>「共創」をテーマにこのサイトは運営しています。</p>
+                <p>互いに助け合い、寄り添って問題解決・改善を行いましょう。</p>
+                <p>サイトに関して不便なことがありましたら、支援員にご相談ください。</p>
+            </div>
+        </div>
+    </div>
 </main>
+
+
+<?php get_footer(); ?>
