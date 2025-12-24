58e767f10678bc06124a22f722e619a755f0ab66
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2025-12-24 14:25:56 +0900

    navigation

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
             <li class="list__item">
                 <div class="dropDown">
                     <button class="dropDown__btn">
-                        <a href="<?php echo esc_url(home_url('/production')); ?>">Products</a>
+                        <a href="<?php echo esc_url(home_url('/Production')); ?>">Products</a>
                     </button>
                     <div class="dropDown__content">
                         <a href="#" class="dropDown__link">
@@ -36,19 +36,52 @@
                 </div>
             </li>
             <li class="list__item">
-                <a href="<?php echo esc_url(home_url('/manual')); ?>">
-                    Manuals
-                </a>
+                <div class="dropDown">
+                    <button class="dropDown__btn">
+                        <a href="<?php echo esc_url(home_url('/Manual')); ?>">Manuals</a>
+                    </button>
+                    <div class="dropDown__content">
+                        <a href="#" class="dropDown__link">
+                            <span>Process Manuals</span>
+                        </a>
+                    </div>
+                </div>
             </li>
             <li class="list__item">
-                <a href="<?php echo esc_url(home_url('/shift')); ?>">
-                    Shifts
-                </a>
+                <div class="dropDown">
+                    <button class="dropDown__btn">
+                        <a href="<?php echo esc_url(home_url('/Shifts')); ?>">Shifts</a>
+                    </button>
+                    <div class="dropDown__content">
+                        <a href="#" class="dropDown__link">
+                            <span>January Schedule</span>
+                        </a>
+                        <a href="#" class="dropDown__link">
+                            <span>December Schedule</span>
+                        </a>
+                        <a href="#" class="dropDown__link">
+                            <span>November Schedule</span>
+                        </a>
+                    </div>
+                </div>
             </li>
             <li class="list__item">
-                <a href="<?php echo esc_url(home_url('/daily')); ?>">
-                    Daily
-                </a>
+                <div class="dropDown">
+                    <button class="dropDown__btn">
+                        <a href="<?php echo esc_url(home_url('/Montly')); ?>">Monthly</a>
+                    </button>
+                    <div class="dropDown__content">
+                        <a href="#" class="dropDown__link">
+                            <span>January Report</span>
+                        </a>
+                        <a href="#" class="dropDown__link">
+                            <span>December Report</span>
+                        </a>
+                        <a href="#" class="dropDown__link">
+                            <span>November Report</span>
+                        </a>
+                    </div>
+                </div>
             </li>
             <li class="list__item">
                 <a href="<?php echo esc_url(home_url('/case')); ?>">
